d43a8b7c4d16084005b3744d53c251a2b8ca2739
RedSpah <redspah@gmail.com>
2015-04-20 19:34:23 +0200

    version 0.0.4.1
    laser turrets

--- Assets/Scripts/Entity/S_Turret.cs
+++ Assets/Scripts/Entity/S_Turret.cs
@@ -14,38 +14,58 @@ public class S_Turret : MonoBehaviour {
 	public LayerMask PlayerLayer;
 	public LayerMask EndLevel;
 
-
-	public int InitWait = 20;
-	public int Cooldown = 10;
+	public bool GunActive = false;
+	public int GunInitWait = 20;
+	public int GunCooldown = 10;
 	public bool BulletBoostable = false;
 	public float BulletSpeed = 200;
-	public float RotationSpeed = 0.5f;
-	public bool LaserActive = false;
-	public bool GunActive = false;
-	public bool AimThroughWalls = false;
 
+	public float RotationSpeed = 0.5f;
+
+
+	public bool LaserActive = false;
+	public bool ConstLaser = false;
+	public bool TurnWhileLaserIsActive = false;
+	public int LaserDuration = 5;
+	public int LaserInitWait = 20;
+	public int LaserCooldown = 10;
+
+
+
+
+	public bool AimThroughWalls = false;
+	public bool FireAtWalls = false;
+	public float LaserOffset = 0;
 
 	private Quaternion InitAngle;
 
+	private int BulletTimer = 0;
+	private int LaserTimer = 0;
 
 	public bool turretActive = false;
 	private int TimeSeen;
 	private int CooldownTimer;
 	private bool CanSee;
-	private LineRenderer Laser;
+	public LineRenderer AimLaser;
+	public LineRenderer KillLaser;
 
 
 	void Start () {
 
 		//TurretTip = GameObject.Find ("O_BarrelTip").GetComponent<Transform>();
 		InitAngle = gameObject.transform.rotation;
-		Laser = gameObject.GetComponentInChildren<LineRenderer> ();
-		Laser.SetColors (Color.red, Color.red);
-		Laser.SetWidth (0.1f, 0.1f);
+		//AimLaser = GameObject.Find ("TO_AimBeamRenderer").GetComponent<LineRenderer> ();
+		//KillLaser = GameObject.Find ("TO_KillBeamRenderer").GetComponent<LineRenderer> ();
 
-		//LaserHit = GameObject.Find("TO_Hit").GetComponent<Transform>();
-		Laser.enabled = true;
-		Laser.useWorldSpace = true;
+		AimLaser.SetColors (Color.red, Color.red);
+		AimLaser.SetWidth (0.1f, 0.1f);
+		AimLaser.enabled = true;
+		AimLaser.useWorldSpace = true;
+
+		KillLaser.SetColors (Color.red, Color.red);
+		KillLaser.SetWidth (0.1f, 0.1f);
+		KillLaser.enabled = false;
+		KillLaser.useWorldSpace = true;
 
 		CollisionLayer += EndLevel;
 		PlayerLayer += CollisionLayer;
@@ -54,52 +74,88 @@ public class S_Turret : MonoBehaviour {
 
 	void FixedUpdate () {
 		if (turretActive) {
+
 			float len = Vector2.Distance (gameObject.transform.position, PlayerRef.transform.position);
+			RaycastHit2D SeeBeam = Physics2D.Raycast (gameObject.transform.position, PlayerRef.transform.position - gameObject.transform.position, len, CollisionLayer);   
+			CanSee = (SeeBeam.collider == null); 
 
-			RaycastHit2D v;
-			if (LaserActive)
-			{
-				v = Physics2D.Raycast (TurretTip.position, transform.right, Mathf.Infinity, PlayerLayer);
-			}
-			else
-			{
-				v = Physics2D.Raycast (TurretTip.position, transform.right, Mathf.Infinity, CollisionLayer);
-			}
-
-			RaycastHit2D k = Physics2D.Raycast (gameObject.transform.position, PlayerRef.transform.position - gameObject.transform.position, len, CollisionLayer);   
-			CanSee = (k.collider == null); 
+			gameObject.transform.rotation =  Quaternion.Euler(0, 0, gameObject.transform.rotation.eulerAngles.z + LaserOffset);
+			RaycastHit2D AimBeam = Physics2D.Raycast (TurretTip.position, transform.right, Mathf.Infinity, CollisionLayer);
+			gameObject.transform.rotation =  Quaternion.Euler(0, 0, gameObject.transform.rotation.eulerAngles.z - LaserOffset);
+			LaserHit.position = AimBeam.point;
+			AimLaser.SetPosition(0, TurretTip.position);
+			AimLaser.SetPosition(1, LaserHit.position);
+			KillLaser.SetPosition(0, TurretTip.position);
+			KillLaser.SetPosition(1, LaserHit.position);
 
 
-			if (v.collider != null && v.collider.gameObject.tag.Equals ("Player"))
-			{
-				v.collider.gameObject.GetComponent<S_PlayerMovement>().death();
-			}
-
-			LaserHit.position = v.point;
-			Laser.SetPosition(0, TurretTip.position);
-			Laser.SetPosition(1, LaserHit.position);
 
 
-			if (CanSee || AimThroughWalls) {
+
+
+
+
+
+
+
+
+
+			if (CanSee || AimThroughWalls || !(!TurnWhileLaserIsActive && LaserTimer > LaserCooldown)) {
 				Vector3 lookDirection = PlayerRef.transform.position - gameObject.transform.position;
 				float angle = Mathf.Atan2 (lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
 				Quaternion targetRotation = Quaternion.AngleAxis (angle, Vector3.forward);
 				transform.rotation = Quaternion.Slerp (gameObject.transform.rotation, targetRotation, RotationSpeed);
 			}
 
-			if (CanSee) 
+			if (CanSee || FireAtWalls) 
 			{
 				TimeSeen++;
+				LaserTimer++;
+				BulletTimer++;
 			}
 			else {
 				TimeSeen = 0;
+				LaserTimer = 0;
+				BulletTimer = 0;
 			}
 
-			if (TimeSeen > InitWait && (TimeSeen % Cooldown) == 0 && GunActive) {
+			if (TimeSeen > GunInitWait && BulletTimer > GunCooldown && GunActive) {
 				GameObject tempBul = (GameObject)Instantiate ((BulletBoostable) ? BulletBoost : BulletNoBoost, gameObject.transform.position, Quaternion.Euler (0, 0, transform.eulerAngles.z));
 				Vector3 vel = tempBul.transform.rotation * Vector3.right;
 				tempBul.GetComponent<Rigidbody2D> ().AddForce (vel * BulletSpeed);
+				BulletTimer = 0;
 			}
+
+			if (TimeSeen > LaserInitWait && LaserTimer > LaserCooldown && LaserActive)
+			{
+				RaycastHit2D KillBeam;
+				KillLaser.enabled = true;
+				if (LaserOffset != 0)
+				{
+					gameObject.transform.rotation =  Quaternion.Euler(0, 0, gameObject.transform.rotation.eulerAngles.z + LaserOffset);
+					KillBeam = Physics2D.Raycast (TurretTip.position, transform.right, Mathf.Infinity, PlayerLayer);
+					gameObject.transform.rotation =  Quaternion.Euler(0, 0, gameObject.transform.rotation.eulerAngles.z - LaserOffset);
+				}
+				else
+				{
+					KillBeam = Physics2D.Raycast (TurretTip.position, transform.right, Mathf.Infinity, PlayerLayer);
+				}
+
+				if (KillBeam.collider != null && KillBeam.collider.gameObject.tag.Equals ("Player"))
+				{
+					KillBeam.collider.gameObject.GetComponent<S_PlayerMovement>().death();
+				}
+			}
+			else
+			{
+				KillLaser.enabled = false;
+			}
+
+			if (LaserTimer >= LaserCooldown + LaserDuration && !ConstLaser)
+			{
+				LaserTimer = 0;
+			}
+
 		}
 		// remember to credit /u/kreaol n /u/Zwejhajfa in credits and shit
 	
